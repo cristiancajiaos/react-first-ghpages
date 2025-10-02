625f6cf3a77956104d6ee507c4b464df29329505
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-10-02 13:31:18 -0300

    Instalado paquete react-fontawesome, creado listado de iconos-texto en componente Main

--- src/layout/Main.tsx
+++ src/layout/Main.tsx
@@ -1,11 +1,37 @@
 import { Container } from "react-bootstrap";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faGlobe, faCheck, faTimes  } from '@fortawesome/free-solid-svg-icons';
+import type { IconProp } from "@fortawesome/fontawesome-svg-core";
+
+interface IconText {
+  icon: IconProp;
+  text: string;
+}
+
+const iconsTexts: IconText[] = [{
+  icon: faUser,
+  text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+}, {
+  icon: faGlobe,
+  text: 'Morbi vitae est quam.'
+}, {
+  icon: faCheck,
+  text: 'Ut consequat ante et augue ullamcorper malesuada.'
+}, {
+  icon: faTimes,
+  text: 'Nulla vitae convallis lorem.'
+}];
 
 export const Main = () => {
   return (
     <>
       <main>
         <Container fluid className="my-2">
-          <p>Main</p>
+          {iconsTexts.map(({icon, text}) => {
+            return (
+              <p><FontAwesomeIcon icon={icon} /> {text}</p>
+            )
+          })}
         </Container>
       </main>
     </>
